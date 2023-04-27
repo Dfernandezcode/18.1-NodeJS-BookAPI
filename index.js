8f9437a92379f1c80806579a27dfba1a374becef
Daniel Fernandez <dfernandezcode@gmail.com>
2023-04-27 22:14:17 +0200

    compelte

--- index.js
+++ index.js
@@ -42,6 +42,39 @@ router.get("/book/:id", (req, res) => {
     .catch((error) => res.status(500).json(error));
 });
 
+// search by title (or other parameter)
+router.get("/book/title/:title", async (req, res) => {
+  const title = req.params.title;
+
+  try {
+    // const book = await Book.find({ title: title });
+    const book = await Book.find({ title: new RegExp("^" + title.toLowerCase(), "i") });
+    if (book) {
+      res.json(book);
+    } else {
+      res.status(404).json({});
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Endpoint user creation
+router.post("/book", async (req, res) => {
+  try {
+    const book = new Book({
+      title: req.body.title,
+      author: req.body.author,
+      pages: req.body.pages,
+    });
+
+    const createdBook = await book.save();
+    return res.status(201).json(createdBook);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 server.use("/", router);
 server.listen(PORT, () => {
   console.log(`Server levantado en el puerto ${PORT}`);
